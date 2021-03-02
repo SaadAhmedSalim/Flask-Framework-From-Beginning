90494eecf7d7b148fa17dcb53b300d5c9d0f711d
SaadSalim <shuvrohossain8@gmail.com>
2021-03-02 12:00:22 +0600

    variable testing added

---
+++ varibletest.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)

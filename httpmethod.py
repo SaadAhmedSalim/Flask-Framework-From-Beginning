47c4c91b508cc10dec19bd1eccf1da29b6971740
SaadSalim <shuvrohossain8@gmail.com>
2021-03-02 12:31:29 +0600

    updated templates

--- httpmethod.py
+++ httpmethod.py
@@ -1,6 +1,10 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request,render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
 
 @app.route('/welcome/<name>')
 def welcome(name):
